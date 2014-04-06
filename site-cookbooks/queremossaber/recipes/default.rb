963faa240eda7ec8646891ffe633a64d1288f353
Vitor Baptista <vitor@vitorbaptista.com>
2014-04-06 18:05:38 -0300

    Adds initial queremossaber cookbook and installs Docker

---
+++ site-cookbooks/queremossaber/recipes/default.rb
@@ -0,0 +1,11 @@
+include_recipe "docker"
+
+DOCKER_GROUP = node[:docker][:group] || "docker"
+
+user node[:queremossaber][:user]
+
+group DOCKER_GROUP do
+  action :modify
+  members node[:queremossaber][:user]
+  append true
+end
