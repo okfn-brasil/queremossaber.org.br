74e6927b20c6935c4cedab2d77afe9d1b4b69ca5
Vitor Baptista <vitor@vitorbaptista.com>
2014-04-07 13:46:18 -0300

    Configures and runs queremossaber's docker container

--- site-cookbooks/queremossaber/recipes/docker.rb
+++ site-cookbooks/queremossaber/recipes/docker.rb
@@ -1,5 +1,17 @@
+node.default['docker']['image_cmd_timeout'] = 9999
+
 docker_image 'queremossaber_data' do
   source 'https://raw.githubusercontent.com/okfn-brasil/queremossaber-docker/master/data/Dockerfile'
-  tag 'queremossaber_data'
+  rm true
   action :build_if_missing
+end
+
+docker_image 'vitorbaptista/queremossaber'
+
+docker_container 'queremossaber_data'
+
+docker_container 'vitorbaptista/queremossaber' do
+  detach true
+  port '80:80'
+  volumes_from 'queremossaber_data'
 end
